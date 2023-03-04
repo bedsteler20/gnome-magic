4378348c7c305619eeadbfa88a606bf268666f7d
Cameron Dehning <bedsteler2.0@gmail.com>
2023-03-04 08:33:41 -0600

    _

--- src/blueprint/lsp.ts
+++ src/blueprint/lsp.ts
@@ -7,18 +7,24 @@ export class BlueprintLanguageClient extends LanguageClient {
   constructor() {
     const args: string[] = [];
     let cmd = "blueprint-compiler";
-    for (const wsFolder of vscode.workspace.workspaceFolders ?? []) {
-      const dir = wsFolder.uri.fsPath;
-      const wsBlp = path.join(
-        dir,
-        "subprojects",
-        "blueprint-compiler",
-        "blueprint-compiler.py"
-      );
-      if (fs.existsSync(wsBlp)) {
-        cmd = "python3";
-        args[0] = wsBlp;
-        break;
+    if (
+      vscode.workspace
+        .getConfiguration("gnome-magic")
+        .get("useWorkspaceBlueprintLsp")
+    ) {
+      for (const wsFolder of vscode.workspace.workspaceFolders ?? []) {
+        const dir = wsFolder.uri.fsPath;
+        const wsBlp = path.join(
+          dir,
+          "subprojects",
+          "blueprint-compiler",
+          "blueprint-compiler.py"
+        );
+        if (fs.existsSync(wsBlp)) {
+          cmd = "python3";
+          args[0] = wsBlp;
+          break;
+        }
       }
     }
     super(
